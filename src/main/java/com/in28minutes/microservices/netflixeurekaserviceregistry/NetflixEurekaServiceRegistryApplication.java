ff03326de6d8c0c4aee2af7b1863aba63b05ff57
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2018-12-14 18:53:52 +0530

    First Copy of Eureka Component Files

---
+++ src/main/java/com/in28minutes/microservices/netflixeurekaserviceregistry/NetflixEurekaServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.in28minutes.microservices.netflixeurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaServiceRegistryApplication.class, args);
+	}
+
+}
+
